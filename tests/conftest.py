797545e2e7d5fa125ec8edcb2588504def8f5578
Sergey Sokov <sokov.tlt@gmail.com>
2025-06-23 17:07:19 +0200

    fixed fixture - added --headless=new for CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,6 +11,11 @@ def driver():
     options.add_argument("--window-size=1920,1080")
     if os.getenv("HEADLESS") == "1":
         options.add_argument("--headless")
+    if os.getenv("CI") == "true":
+        options.add_argument("--headless=new")  # современный headless
+        options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
